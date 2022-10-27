279841c408a6a2fbf02b3c5e0d4d49a7436d0e66
abouget <alexandre.bouget@etu.u-bordeaux.fr>
2022-10-27 17:44:38 +0200

    add micro-setup

---
+++ micro_setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+url="https://github.com/MisterMine01/tohutoro/releases/download/v1.0/tohutoro.zip"
+curl -L -o /tmp/tohutoro.zip $url
+unzip /tmp/tohutoro.zip -d /tmp/tohutoro
+if [ ! -d ~/.tohutoro ] ; then
+    mkdir ~/.tohutoro
+else 
+    echo "Directory ~/.tohutoro already exists"
+fi
+cp -r /tmp/tohutoro/* ~/.tohutoro
+
+chmod +x -R ~/.tohutoro
+rm -rf /tmp/tohutoro
+rm /tmp/tohutoro.zip
+~/.tohutoro/setup.sh
