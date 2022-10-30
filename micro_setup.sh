14704a0029def79926049eece42ca63b995c545a
MisterMine01 <pokemovie06@gmail.com>
2022-10-30 09:41:05 +0100

    change micro_setup

--- micro_setup.sh
+++ micro_setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-url="https://github.com/MisterMine01/tohutoro/releases/download/v1.0/tohutoro.zip"
+url="https://github.com/MisterMine01/tohutoro/releases/download/v1.1/tohutoro.zip"
 curl -L -o /tmp/tohutoro.zip $url
 unzip /tmp/tohutoro.zip -d /tmp/tohutoro
 if [ ! -d ~/.tohutoro ] ; then
